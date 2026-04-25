3737dbf5bf87557440df147e5c2755eded4dab4a
Lénaïc Grolleau <lenaic@lenaic.me>
2026-04-25 04:24:09 +0200

    Added missing exports

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 // Public API for TagStrip library
 
-// Core encoding/decoding functions
 export { encode } from './core/encoder.js';
 export { decodeBits } from './core/decoder.js';
 
-// Constants for external use
 export { SHORT_MAX_ID, LONG_MAX_ID } from './core/constants.js';
 
-// Scanner API (browser only)
-// Note: Scanner modules are not exported here as they require browser APIs
-// They will be imported separately when needed in browser environments
+export { toGrayscale, binarizeOtsu, binarizeSimple } from './scanner/binarize.js';
+export {
+  detectTagInSignal,
+  searchBandsGrayscale,
+  searchBands,
+} from './scanner/locator.js';
+export { processFrame, VotingBuffer, Scanner, createScanner } from './scanner/pipeline.js';
